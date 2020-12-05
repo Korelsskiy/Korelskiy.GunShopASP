22944c10d9d677811ae64225d29eabd63931a775
Korelsskiy <68390165+Korelsskiy@users.noreply.github.com>
2020-12-05 04:15:44 +0300

    Созданы макеты всех страниц проекта

---
+++ Pages/Products/Deatails.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Korelskiy.GunShopASP.Pages.Products
+{
+    public class DeatailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
